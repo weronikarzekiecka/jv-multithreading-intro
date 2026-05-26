0c3bd912e0e51922de2931b344cb13189078afbe
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2026-05-26 12:19:23 +0200

    solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,29 @@
-package core.basesyntax;
-
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread numbersThread = new NumbersThread();
+
+        Thread daemonThread = new Thread(new DaemonTask());
+        daemonThread.setDaemon(true);
+
+        numbersThread.start();
+        daemonThread.start();
+    }
+
+    public static class NumbersThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i <= 20; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static class DaemonTask implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("I am the daemon thread!");
+            }
+        }
     }
 }
